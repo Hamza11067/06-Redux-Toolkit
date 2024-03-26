e0ec305e085bfa191cef10497b86de8245bafc02
Hamza11067 <hamzakhalid1067@gmail.com>
2024-03-27 00:49:59 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import TodoForm from './components/TodoForm'
+import Todos from './components/Todos'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Learn about redux toolkit</h1>
+      <TodoForm />
+      <Todos />
+    </>
+  )
+}
+
+export default App
